07e6556c0934e9438021094ccfd78e662290fdca
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-06-16 12:34:12 +0300

    исправить ошибки

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const NOT_FOUND = 404;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,5 +19,9 @@ app.use((req, res, next) => {
 
 app.use("/users", require('./routes/users'));
 app.use("/cards", require('./routes/cards'));
+
+app.use("*", (req, res, next) => {
+  next(res.status(NOT_FOUND).send({ message: `Страница не найдена` }));
+});
 
 app.listen(PORT);
