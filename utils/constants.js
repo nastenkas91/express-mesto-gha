53c698ed5b48adfdffac6d801eb04b33ff71a2e2
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-07-04 22:44:21 +0300

    добавить cors

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,7 @@
 const URL_REGEX = /^(https?:\/\/(www\.)?([a-zA-z0-9-]{1}[a-zA-z0-9-]*\.?)*\.{1}([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:?#[]@!\$&'\(\)\*\+,;=])*)/;
 
-module.exports = URL_REGEX;
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+module.exports = {
+  URL_REGEX, DEFAULT_ALLOWED_METHODS,
+};
