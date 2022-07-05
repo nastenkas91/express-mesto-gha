9e21cb84525c658de352cd50e61fb800ca2f1a86
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-07-05 18:09:46 +0300

    добавить cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
-const cors = require('cors');
+const cors = require('./middlewares/cors');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const routerUsers = require('./routes/users');
@@ -12,7 +12,6 @@ const routerCards = require('./routes/cards');
 const NotFound = require('./errors/NotFound');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const cors = require('./middlewares/cors');
 
 const URL_REGEX = require('./utils/constants');
 
