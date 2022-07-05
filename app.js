79467f5daf7ce8193e5ac2168907746a35297326
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-07-05 13:28:09 +0300

    добавить cors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
   if (req.method === 'OPTIONS') {
