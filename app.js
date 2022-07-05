c59003612176ff286d35a3a16eac41522b34227c
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-07-05 14:42:13 +0300

    добавить cors

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     return res.send(200);
   }
