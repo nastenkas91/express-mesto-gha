c9c32cec61b2770394bd81b7891fa8ffe8670a4c
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-07-05 16:55:42 +0300

    добавить cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://mesto-project.nomoredomains.sbs');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
   if (req.method === 'OPTIONS') {
