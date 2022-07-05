bb8f4eaa019ce33fa775f1ef1caba5995b5b62d5
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-07-05 17:56:59 +0300

    добавить cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,19 +1,22 @@
-// const allowedCors = [
-//   'http://mesto-project.nomoredomains.sbs',
-//   'https://mesto-project.nomoredomains.sbs'];
+const allowedCors = [
+  'http://mesto-project.nomoredomains.sbs',
+  'https://mesto-project.nomoredomains.sbs'];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const cors = (req, res, next) => {
-  // const { origin } = req.headers;
+  const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Origin', '*');
-  // if (allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', origin);
-  // }
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   if (method === 'OPTIONS') {
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
