5a08de25840321b49d61b333ffa869c8deec54c4
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-07-06 14:53:56 +0300

    добавить router

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
+const helmet = require('helmet');
+const routes = require('./routes/index');
 const cors = require('./middlewares/cors');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
-const routerUsers = require('./routes/users');
-const routerCards = require('./routes/cards');
 const NotFound = require('./errors/NotFound');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -26,36 +26,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(cors);
 
+app.use(helmet());
+
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(URL_REGEX),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-app.use('/users', auth, routerUsers);
-app.use('/cards', auth, routerCards);
-
-app.use('*', (req, res, next) => {
-  next(new NotFound('Страница не найдена'));
-});
+app.use(routes);
 
 app.use(errorLogger);
 
